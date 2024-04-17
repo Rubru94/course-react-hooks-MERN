0669a1565faf2dbe0debf2e5607dad3613c6b47e
Ruben Rubio Perucha <ruben.rubioperucha@plexus.es>
2024-04-17 16:01:48 +0200

    feat(03-counter-app-vite): Hello world

--- react/03-counter-app-vite/src/main.tsx
+++ react/03-counter-app-vite/src/main.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+function App() {
+  // document.createElement...
+  return <h1>Hello World!!!</h1>;
+}
+
+const App2 = () => <h1>Hello World!!! from arrow function</h1>;
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
+    <App2 />
   </React.StrictMode>,
 );
