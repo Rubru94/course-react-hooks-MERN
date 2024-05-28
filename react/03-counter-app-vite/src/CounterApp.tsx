b6a6af8e0144f8eea74d44ed51f618c196eba61b
Ruben Rubio Perucha <ruben.rubioperucha@plexus.es>
2024-05-28 12:44:58 +0200

    feat(03-counter-app-vite): button click event

--- react/03-counter-app-vite/src/CounterApp.tsx
+++ react/03-counter-app-vite/src/CounterApp.tsx
@@ -1,12 +1,29 @@
+import { MouseEvent } from 'react';
+
 export interface CounterAppProps {
   value?: number;
 }
 
 export const CounterApp = ({ value }: CounterAppProps) => {
+  /* const handleAdd = (event: MouseEvent, value: number) => {
+    console.log(value);
+  }; */
+
+  const handleAdd = (event: MouseEvent) => {
+    if (value) value += 1;
+    console.log(event);
+    console.log(value);
+  };
+
   return (
     <>
       <h1>CounterApp</h1>
-      <h2>{value}</h2>
+      <h2> {value} </h2>
+      {/* equivalent --> it can be simplified by leaving only the reference to the function that will receive the same arguments in same order */}
+      {/* <button onClick={(event) => handleAdd(event)}>+1</button> */}
+      <button onClick={handleAdd}>+1</button>
+
+      {/* <button onClick={(event) => handleAdd(event, 1)}>+1</button> */}
     </>
   );
 };
--- react/03-counter-app-vite/src/main.tsx
+++ react/03-counter-app-vite/src/main.tsx
@@ -1,22 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-// import AppHelloWorld from './hello-world-app'; /** default export */
-// import { App, App2, HelloWorldApp } from './HelloWorldApp';
-import { FirstApp } from './FirstApp';
 import { CounterApp } from './CounterApp';
 
 import './styles.scss';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    {/* <App /> */}
-    {/* <HelloWorldApp /> */}
-    {/* <AppHelloWorld /> --> default export */}
-    {/* <App2 /> */}
-
-    {/* <FirstApp title="Hello world !!!" subtitle={123} /> */}
-    <FirstApp title="Hello world" num={10} />
     <CounterApp value={10}></CounterApp>
   </React.StrictMode>,
 );
