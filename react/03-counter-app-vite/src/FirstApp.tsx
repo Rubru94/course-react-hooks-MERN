c9eaa80d17de4c5d05ab50915e598620547b7560
Ruben Rubio Perucha <ruben.rubioperucha@plexus.es>
2024-05-28 08:39:23 +0200

    feat(03-counter-app-vite): Fragment

--- react/03-counter-app-vite/src/FirstApp.tsx
+++ react/03-counter-app-vite/src/FirstApp.tsx
@@ -1,3 +1,22 @@
+/* import { Fragment } from 'react';
+
 export const FirstApp = () => {
-  return <h1>FirstApp</h1>;
+  return (
+    <Fragment>
+      <h1>FirstApp</h1>
+      <p>Subtitle</p>
+    </Fragment>
+  );
+};
+ */
+
+// <> Same behavior as importing Fragment but without import
+
+export const FirstApp = () => {
+  return (
+    <>
+      <h1>FirstApp</h1>
+      <p>Subtitle</p>
+    </>
+  );
 };
