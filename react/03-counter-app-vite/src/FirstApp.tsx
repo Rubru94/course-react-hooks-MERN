95d87559f52bdc54672ed6649723b8cbb8c3ac02
Ruben Rubio Perucha <ruben.rubioperucha@plexus.es>
2024-05-28 09:05:15 +0200

    feat(03-counter-app-vite): Print variables in html

--- react/03-counter-app-vite/src/FirstApp.tsx
+++ react/03-counter-app-vite/src/FirstApp.tsx
@@ -1,21 +1,24 @@
-/* import { Fragment } from 'react';
-
-export const FirstApp = () => {
-  return (
-    <Fragment>
-      <h1>FirstApp</h1>
-      <p>Subtitle</p>
-    </Fragment>
-  );
+const objMessage = {
+  message: 'Hello world !!',
+  title: 'Greeting',
 };
- */
 
-// <> Same behavior as importing Fragment but without import
+const getMessage = (msg?: string): string => {
+  return msg ?? objMessage.message;
+};
+
+const getResult = (a: number, b: number): number => {
+  return a + b;
+};
 
 export const FirstApp = () => {
   return (
     <>
-      <h1>FirstApp</h1>
+      <h1>First app</h1>
+      <h2>Message: {getMessage('Hi')}</h2>
+      <h3>Result: {getResult(2, 5)}</h3>
+      {/* You cannot print objects directly in react (you can use JSON.stringify), nor async function results. */}
+      {/* <code>{JSON.stringify(objMessage)}</code> */}
       <p>Subtitle</p>
     </>
   );
