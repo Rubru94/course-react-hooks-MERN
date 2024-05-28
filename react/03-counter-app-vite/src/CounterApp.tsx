e683ae2b51eab1adfc5b5c4705591f67dcb5a472
Ruben Rubio Perucha <ruben.rubioperucha@plexus.es>
2024-05-28 13:08:49 +0200

    feat(03-counter-app-vite): hooks - handleSubtract & handleReset

--- react/03-counter-app-vite/src/CounterApp.tsx
+++ react/03-counter-app-vite/src/CounterApp.tsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'; // nomenclature --> if it starts with "use" it is a hook
-import { MouseEvent } from 'react';
 
 export interface CounterAppProps {
   value?: number;
 }
 
 export const CounterApp = ({ value }: CounterAppProps) => {
+  console.log('render'); // When status changes, the component is executed again
+
   const [counter, setCounter] = useState(value);
 
   const handleAdd = () => {
@@ -13,13 +14,19 @@ export const CounterApp = ({ value }: CounterAppProps) => {
     // setCounter((c) => c + 1);
   };
 
+  const handleSubtract = () => setCounter(counter - 1);
+
+  const handleReset = () => setCounter(value);
+
   return (
     <>
       <h1>CounterApp</h1>
       <h2> {counter} </h2>
       {/* equivalent --> it can be simplified by leaving only the reference to the function that will receive the same arguments in same order */}
       {/* <button onClick={(event) => handleAdd(event)}>+1</button> */}
-      <button onClick={handleAdd}>+1</button>
+      <button onClick={handleAdd}> +1 </button>
+      <button onClick={handleSubtract}> -1 </button>
+      <button onClick={handleReset}> Reset </button>
 
       {/* <button onClick={(event) => handleAdd(event, 1)}>+1</button> */}
     </>
