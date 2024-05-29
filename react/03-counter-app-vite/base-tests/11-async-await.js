1d874f4d5ebb2b13aea2152b2ac48f392c2db559
Ruben Rubio Perucha <ruben.rubioperucha@plexus.es>
2024-05-29 15:06:30 +0200

    feat(03-counter-app-vite-testing): 11-async-await

--- react/03-counter-app-vite/base-tests/11-async-await.js
+++ react/03-counter-app-vite/base-tests/11-async-await.js
@@ -1,45 +1,15 @@
-// async - await
+// https://developers.giphy.com/ >> Docs >> Create an Api key
 
-// const getImagePromise = () => new Promise((resolve) => resolve('image'));
-// getImagePromise().then(console.log)
-
-// async
-
-/* const getImage = async () => {
-  return 'image';
-};
-getImage().then(console.log); */
-
-/* const getImage = async () => {
-  const apiKey = 'Ho51Dc0beMaMoLFsb14AVa96Np4lEcM0';
-  const res = await fetch(
-    `https://api.giphy.com/v1/gifs/random?api_key=${apiKey}`
-  );
-  const { data } = await res.json();
-  const { url } = data.images.original;
-
-  const img = document.createElement('img');
-  img.src = url;
-  document.body.append(img);
-}; */
-
-// To handle errors in an async function, use try-catch.
-
-const getImage = async () => {
+export const getImage = async () => {
   try {
     const apiKey = 'Ho51Dc0beMaMoLFsb14AVa96Np4lEcM0';
-    const res = await fetch(
-      `https://api.giphy.com/v1/gifs/random?api_key=${apiKey}`
-    );
+    const res = await fetch(`https://api.giphy.com/v1/gifs/random?api_key=${apiKey}`);
     const { data } = await res.json();
     const { url } = data.images.original;
 
-    const img = document.createElement('img');
-    img.src = url;
-    document.body.append(img);
+    return url;
   } catch (error) {
     console.error(error);
+    return 'Image not found';
   }
 };
-
-getImage();
