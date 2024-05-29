bf91c4179e0dea939df37bbd3b35a4a328ca6461
Ruben Rubio Perucha <ruben.rubioperucha@plexus.es>
2024-05-29 16:01:30 +0200

    feat(03-counter-app-vite-testing): Configure react testing settings

--- react/03-counter-app-vite/babel.config.cjs
+++ react/03-counter-app-vite/babel.config.cjs
@@ -2,5 +2,9 @@
 
 // eslint-disable-next-line no-undef
 module.exports = {
-  presets: [['@babel/preset-env', { targets: { node: 'current' } }]],
+  presets: [
+    ['@babel/preset-env', { targets: { esmodules: true } }],
+    ['@babel/preset-react', { runtime: 'automatic' }],
+    '@babel/preset-flow',
+  ],
 };
--- react/03-counter-app-vite/jest.config.js
+++ react/03-counter-app-vite/jest.config.js
@@ -2,7 +2,7 @@ export default {
   /* transform: {
     '^.+\\.ts?$': 'ts-jest',
   }, */
-  testEnvironment: 'node',
+  testEnvironment: 'jest-environment-jsdom',
   /* testRegex: './src/.*\\.(test|spec)?\\.(ts|ts)$', */
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   /* roots: ['<rootDir>/src'], */
