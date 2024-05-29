9735e38b4e7bfd1413f0d87a4bc55ceb7af8d434
Ruben Rubio Perucha <ruben.rubioperucha@plexus.es>
2024-05-29 09:42:14 +0200

    feat(03-counter-app-vite-testing): 02-template-strings tests

--- react/03-counter-app-vite/base-tests/02-template-strings.js
+++ react/03-counter-app-vite/base-tests/02-template-strings.js
@@ -1,26 +1,3 @@
-// template strings
-
-const name = 'John';
-const surname = 'Doe';
-
-// const fullName = name + ' ' + surname;
-const fullName = `${name} ${surname}`;
-const fullNameLineBreak = `
-${name} 
-${surname}
-${1 + 2}`;
-const getGreetingArrow = () => '[=>] Hello World !! [=>]';
-const functions = `${getGreeting()} - ${getGreetingArrow()}`;
-
-function getGreeting() {
-  return 'Hello World !!';
-}
-
-function getGreetingTo(name) {
+export function getGreetingTo(name) {
   return `Hello ${name} !!`;
 }
-
-console.log(fullName);
-console.log(fullNameLineBreak);
-console.log(functions);
-console.log(`Greetings: ${getGreetingTo(fullName)}`);
